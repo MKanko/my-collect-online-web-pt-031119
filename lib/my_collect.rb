c6eeb6e5ae59831e1efad259eb414dcc88850134
Mark Kanko <mkanko66@gmail.com>
2019-03-18 22:32:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
 
+def my_collect(collection)
+  binding.pry 
+  modified_collection = []
+  item = 0
+  while item < collection.length
+    modified_collection.push yield(collection[item])
+    item += 1
+  end
+end  
+  
+  
+  
 
