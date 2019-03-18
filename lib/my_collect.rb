1a70744aa604db1fd8713731e04b28653fb94b1f
Mark Kanko <mkanko66@gmail.com>
2019-03-18 22:33:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 def my_collect(collection)
-  
+
   modified_collection = []
   item = 0
   binding.pry
   while item < collection.length
-    
+
     modified_collection.push yield(collection[item])
     item += 1
   end
